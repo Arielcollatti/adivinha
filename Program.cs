36f050e971ff545ad70d366284426c4869c34028
arielcollatti <colatti2017@gmail.com>
2023-03-10 22:25:38 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("adivinha");
+
+Console.WriteLine("O que é, o que é...");
+Console.WriteLine("Dá um pulo e se veste de noiva."); 
+Console.ReadKey();
+Console.WriteLine("A pipoca.");
